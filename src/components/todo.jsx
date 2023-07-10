21d2a0641155ee16be1147535bff2e49bb6b9b95
Wilson Dev <wilson.hch@hynkor.com>
2023-07-09 23:42:42 -0500

    Props completada

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/Todos.css';
 import {AiOutlineCloseCircle,AiOutlineCheck} from 'react-icons/ai';
-function Todo ({text}){
+function Todo ({text,completed}){
     return(
-        <div className="todo-container">
+        <div className={completed? 'todo-container completed':'todo-container'}>
             <div className='todo-container-icons'>
                 <AiOutlineCheck className='todo-icon'/>
             </div>
