74ace7fad41bcdd7ed11f4ce8f1b373985a6c3ec
Wilson Dev <wilson.hch@hynkor.com>
2023-07-09 23:37:38 -0500

    Implementando react icons

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../styles/Todos.css';
+import {AiOutlineCloseCircle,AiOutlineCheck} from 'react-icons/ai';
 function Todo ({text}){
     return(
         <div className="todo-container">
+            <div className='todo-container-icons'>
+                <AiOutlineCheck className='todo-icon'/>
+            </div>
             <div className="todo-text">
                 {text}
             </div>
-            <div className="todo-icon">
-                Eliminar
+            <div className="todo-container-icons">
+                <AiOutlineCloseCircle className='todo-icon'/>
             </div>
         </div>
     )
