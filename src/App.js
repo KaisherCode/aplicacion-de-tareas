4a35ab1efa2dd0c4faf9bf81dafd24f60c549bce
Wilson Dev <wilson.hch@hynkor.com>
2023-07-09 21:57:44 -0500

    Logo y color de fondo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-//import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className="App-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={freeCodeCampLogo} className='freecodecamp-logo'/>
+      </div>
     </div>
   );
 }
