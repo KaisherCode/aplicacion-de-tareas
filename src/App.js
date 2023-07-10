d88fae8e2355eff1fb9c7381233bdddd2efa4614
Wilson Dev <wilson.hch@hynkor.com>
2023-07-09 22:29:31 -0500

    Lista de tareas

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import freeCodeCampLogo from './images/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App-tareas">
+    <div className="App-todos">
       <div className='freecodecamp-logo-contenedor'>
         <img src={freeCodeCampLogo} className='freecodecamp-logo'/>
+      </div>
+      <div className='main-task-list'>
+        <h1>My tasks</h1>
       </div>
     </div>
   );
