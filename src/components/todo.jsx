742d3633c438a9d2aa5a7323022c847770216b44
Wilson Dev <wilson.hch@hynkor.com>
2023-07-10 00:53:06 -0500

    Nuevos props para tarea

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import '../styles/Todos.css';
-import {AiOutlineCloseCircle,AiOutlineCheck} from 'react-icons/ai';
-function Todo ({text,completed}){
-    return(
-        <div className={completed? 'todo-container completed':'todo-container'}>
+import { AiOutlineCloseCircle, AiOutlineCheck } from 'react-icons/ai';
+function Todo({ id, text, completed, todoComplete, deleteTodo }) {
+    return (
+        <div className={completed ? 'todo-container completed' : 'todo-container'}>
             <div className='todo-container-icons'>
-                <AiOutlineCheck className='todo-icon '/>
+                <AiOutlineCheck className='todo-icon ' />
             </div>
-            <div className="todo-text">
+            <div
+                className="todo-text"
+                onClick={() => todoComplete(id)}
+            >
                 {text}
             </div>
-            <div className="todo-container-icons">
-                <AiOutlineCloseCircle className='todo-icon'/>
+            <div
+                className="todo-container-icons"
+                onClick={() => deleteTodo(id)}
+            >
+                <AiOutlineCloseCircle className='todo-icon' />
             </div>
         </div>
     )
 }
-export {Todo}
+export { Todo }
