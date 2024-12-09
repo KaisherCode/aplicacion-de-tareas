4d266d4a41c222d536b79bc49765ab3a9f8a71a7
Wilson Dev <wilson.hch@hynkor.com>
2024-12-09 10:02:03 -0500

    refactor (TodoForm): space refactoring

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import '../styles/TodoForm.css';
-import {AiOutlinePlusCircle} from 'react-icons/ai'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 function TodoForm(props) {
 
-    const [input, setInput] = useState('')
-    const handleChange = (event) => {
-        setInput(event.target.value)
-    }
+  const [input, setInput] = useState('')
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
 
-    const handleSend = (event) => {
-        event.preventDefault()
-        const newTodo = {
-            id: uuidv4(),
-            text: input,
-            completed: false
-        }
-        props.onSubmit(newTodo)
+  const handleSend = (event) => {
+    event.preventDefault()
+    const newTodo = {
+      id: uuidv4(),
+      text: input,
+      completed: false
     }
+    props.onSubmit(newTodo)
+  }
 
-    return (
-        <form className="todo-form" onSubmit={handleSend}>
-            <input
-                required
-                className="todo-input"
-                type="text"
-                placeholder="Write a todo"
-                name="text"
-                onChange={handleChange}
-            />
-            <button className="todo-button"><AiOutlinePlusCircle/></button>
-        </form>
-    )
+  return (
+    <form className="todo-form" onSubmit={handleSend}>
+      <input
+        required
+        className="todo-input"
+        type="text"
+        placeholder="Write a todo"
+        name="text"
+        onChange={handleChange}
+      />
+      <button className="todo-button"><AiOutlinePlusCircle /></button>
+    </form>
+  )
 }
 export { TodoForm }
